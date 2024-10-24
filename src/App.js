5302c3344ded2ca99dec2e2d6b49b300fef641f5
Esmatullah Hadi <esmatullahahdi@gmail.com>
2024-10-24 23:42:05 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Profile from "./assets/images/pic.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  render() {
+    let helloWorld = "Welcome to the road to learn react";
+    const profileStyle = { width: "100px", borderRadius: "50%" };
+    return (
+      <div className="App">
+        <h1>HackerNews</h1>
+        <p>{helloWorld}</p>
+        <div style={{ width: "800px", margin: "auto" }}>
+          <table>
+            <tr>
+              <td>
+                <img src={Profile} style={profileStyle} />
+              </td>
+              <td>Esmatullah</td>
+              <td>Hadi</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
